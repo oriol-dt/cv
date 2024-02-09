1caa8098dc1280692a53fe06be625172563e3e38
Oriol Dueñas <informatic@sivortex.com>
2024-02-09 14:22:56 +0100

    Actualizo JS

--- js/main-script.js
+++ js/main-script.js
@@ -4,3 +4,8 @@ var fechaActual = new Date();
 var day = fechaActual.getDate();
 var month = fechaActual.toLocaleDateString('es-ES', { month: 'long' });
 var year = fechaActual.getFullYear();
+
+var edad = fechaActual.getFullYear() - fechaNacimiento.getFullYear();
+
+const ageSpan = document.getElementById("edad");
+ageSpan.innerHTML = `${edad} años`
